0706a2ef15915aacbf5870a133d25d0a23b612c4
Elero Daniel <danixeee@gmail.com>
2017-11-04 18:11:03 +0100

    da

--- solution.py
+++ solution.py
@@ -40,9 +40,18 @@ def expencive_grid(func):
 
 
 def logic1(func):
+    """
+    battery power on
+    """
     def func_wrapper(msg):
         resp = func(msg)
         # TODO: Import your logic here
+        if msg.buying_price == 8:
+            if msg.selling_price > 0:
+                resp.power_reference = 0.25
+            else:
+                resp.power_reference = msg.current_load - msg.solar_production
+
         return resp
     return func_wrapper
 
@@ -50,12 +59,17 @@ def logic2(func):
     def func_wrapper(msg):
         resp = func(msg)
         # TODO: Import your logic here
+        if msg.bessOverload == True:
+            resp.load_two = False
+            resp.load_three = True
         return resp
     return func_wrapper
 
 def logic3(func):
     def func_wrapper(msg):
         resp = func(msg)
+        if msg.bessSOC < 0.6:
+            resp.power_reference = -6.0
         # TODO: Import your logic here
         return resp
     return func_wrapper
@@ -75,8 +89,8 @@ def logic4(func):
 @expencive_grid
 @logic1
 @logic2
-@logic3
-@logic4
+# @logic3
+# @logic4
 def get_result(msg):
     return ResultsMessage(data_msg=msg,
                           load_one=True,
