63b75b2475c82a8a29b15b16d6e9358d3d5b6d9b
AirmiX <milan_sovic@outlook.com>
2017-11-04 21:19:22 +0100

    Solution WIP

--- solution.py
+++ solution.py
@@ -87,3 +87,82 @@ def get_result(msg):
 
 
 
+###
+
+# DELTA = 0.4
+
+# BATTERY_MAX_POWER = 6.0
+# BATTERY_MAX_POWER_PER_HOUR = 0.6
+
+# LOAD_MAX_POWER = 8.0
+# LOAD_DEVIATION = 0.2
+# LOAD_ONE_POWER_SHARE = 0.2
+# LOAD_TWO_POWER_SHARE = 0.5
+# LOAD_THREE_POWER_SHARE = 0.3
+
+# POSITIVE_SELLING_PRICE = 3
+# CHEAP_BUYING_PRICE = 3
+# EXPENSIVE_BUYING_PRICE = 8
+
+
+# def worker(msg: DataMessage) -> ResultsMessage:
+#     """TODO: This function should be implemented by contestants."""
+#     # Details about DataMessage and ResultsMessage objects can be found in /utils/utils.py
+
+#     # Data Message information
+
+#     is_buying_price_cheap = msg.buying_price == CHEAP_BUYING_PRICE
+#     is_selling_price_positive = msg.selling_price == POSITIVE_SELLING_PRICE
+
+#     is_grid_status_on = msg.grid_status
+#     load_and_solar_diff = float(msg.current_load - msg.solar_production)
+#     main_grid_power = float(msg.mainGridPower)
+
+#     is_battery_overload = msg.bessOverload
+#     is_buttery_prepared_for_electricity_loss = msg.bessSOC > BATTERY_MAX_POWER_PER_HOUR
+
+#     #
+
+#     load_one = True
+
+#     load_three = is_buying_price_cheap and is_grid_status_on is True
+
+#     power_reference = 0.0
+
+#     if msg.grid_status is False:
+
+# # 20 posto od celog koliko je ako samo rade one and two
+#         load_two = msg.current_load * (1 - LOAD_TWO_POWER_SHARE) < BATTERY_MAX_POWER - DELTA
+
+#         power_reference = load_and_solar_diff
+
+#     else:
+
+#         load_two = True
+
+#         if is_buttery_prepared_for_electricity_loss:
+
+#             if not is_buying_price_cheap:
+#                 power_reference = load_and_solar_diff if load_and_solar_diff < 6 else BATTERY_MAX_POWER
+
+#             # else:
+#             #
+#             #     if is_selling_price_positive and msg.mainGridPower < 0:
+#             #         power_reference = -main_grid_power  # if msg.mainGridPower > -6 else 6
+
+#         else:
+
+#             if is_buying_price_cheap:
+#                 power_reference = -6.0
+
+#     pv_mode = PVMode.ON
+
+#     return ResultsMessage(data_msg=msg,
+#                           load_one=load_one,
+#                           load_two=load_two,
+#                           load_three=load_three,
+#                           power_reference=power_reference,
+#                           pv_mode=pv_mode)
+
+
+
